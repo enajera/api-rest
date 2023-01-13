53b465f48d3d67b24ca2e23b62c3be9c451b3f94
Elvin Nájera <elvin7n@gmail.com>
2023-01-13 02:08:10 -0600

    Aplicando terraform y gitlab CI/CD

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = "aws_access_key"
+}
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = "aws_secret_key"
+}
